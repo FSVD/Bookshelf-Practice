4efd5e316eb8288b61b465c894e325df84fe1755
FSVD <fabio.schettino.01@gmail.com>
2017-05-24 17:18:23 +0200

    Modular application structure implemented

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+process.env['NODE_PATH']="./lib";
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -19,12 +17,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Run db when application start
+// run db when application start
 var db = require('./config/db');
 
-// set API access point file
-var accessPoints = require('./routes/access_points');
-app.use(accessPoints);
+// import application's modules
+var appModules = require('./app.modules');
+app.use(appModules);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ app.modules.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+// Including modules:
+var index = require('./lib/index');
+var author = require('./lib/author');
+var book = require('./lib/book');
+var genre = require('./lib/genre');
+
+app.use(index);
+app.use(author);
+app.use(book);
+app.use(genre)
+
+module.exports = app;
