2e23d22298bdc963ce2704f12f5ecbdf1e7f14ee
FSVD <fabio.schettino.01@gmail.com>
2017-05-25 16:07:06 +0200

    Sintax updates

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'lib/_commons/views'));
 app.set('view engine', 'jade');
 
 // run db when application start
